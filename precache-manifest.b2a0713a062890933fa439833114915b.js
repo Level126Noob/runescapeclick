118aa4b7a40c55543451dc6dfbf53e81f5b302d8
Travis Abraham <abe.travis@outlook.com>
2019-09-21 09:55:38 -0700

    Updates

---
+++ precache-manifest.b2a0713a062890933fa439833114915b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4e7851e2980a6ba34d3e246cc411c2",
+    "url": "/runescapeclick/index.html"
+  },
+  {
+    "revision": "0cd369094587514a9a14",
+    "url": "/runescapeclick/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5c523cdfddd9bbc0ea25",
+    "url": "/runescapeclick/static/js/2.08c861bd.chunk.js"
+  },
+  {
+    "revision": "0cd369094587514a9a14",
+    "url": "/runescapeclick/static/js/main.86e51d0f.chunk.js"
+  },
+  {
+    "revision": "fadeb12f226778606b61",
+    "url": "/runescapeclick/static/js/runtime-main.e1c65b02.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/runescapeclick/static/media/logo.25bf045c.svg"
+  }
+]);
